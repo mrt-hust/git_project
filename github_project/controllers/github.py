e45853ad2d72b05630fefab3c3fdcc8dd0388000
AceXXX <ace@trueplus.vn>
2018-01-16 17:47:17 +0700

    fix

--- github_project/controllers/github.py
+++ github_project/controllers/github.py
@@ -11,43 +11,42 @@ class GithubController(http.Controller):
         web_hook = request.env['github_project.web_hook'].search([], limit=1)
         web_hook = web_hook[0] if len(web_hook) > 0 else False
 
-        if web_hook:
+        if not web_hook:
+            return "ERROR! No config Github-Webhook!"
 
-            try:
-                github = OAuth2Session(web_hook.client_id)
-            except Exception as e:
-                return str(e)
+        try:
+            github = OAuth2Session(web_hook.client_id)
+        except Exception as e:
+            return str(e)
 
-            request.env.user.write({'github_access_token': access_token})
-            query_access_token = '?access_token=' + str(access_token)
-            api_domain = 'https://api.github.com/'
-            res = github.get(api_domain + 'user' + query_access_token)
-            data = json.loads(res.content.decode('utf-8'))
-            # Get Repositories
-            repos = data.get('repos_url', '')
-            if repos:
-                res = github.get(repos)
-                repo_data = json.loads(res.content.decode('utf-8'))
+        request.env.user.write({'github_access_token': access_token})
+        query_access_token = '?access_token=' + str(access_token)
+        api_domain = 'https://api.github.com/'
+        res = github.get(api_domain + 'user' + query_access_token)
+        data = json.loads(res.content.decode('utf-8'))
+        # Get Repositories
+        repos = data.get('repos_url', '')
+        if repos:
+            res = github.get(repos)
+            repo_data = json.loads(res.content.decode('utf-8'))
 
-                repo_list = []
-                for repo in repo_data:
-                    repo_list.append({'url': repo.get('html_url'), 'name': repo.get('full_name'),
-                                      'owner_id': request.env.user.id})
+            repo_list = []
+            for repo in repo_data:
+                repo_list.append({'url': repo.get('html_url'), 'name': repo.get('full_name'),
+                                  'owner_id': request.env.user.id})
 
-                current_repositories = request.env['github_project.repository']\
-                    .search([('owner_id', '=', request.env.user.id)])
-                existed_repositories = {r.name: r for r in current_repositories}
+            current_repositories = request.env['github_project.repository']\
+                .search([('owner_id', '=', request.env.user.id)])
+            existed_repositories = {r.name: r for r in current_repositories}
 
-                for r in repo_list:
-                    existed_r = existed_repositories.get(r['name'])
-                    if existed_r:
-                        existed_r.write(r)
-                    else:
-                        request.env['github_project.repository'].create(r)
+            for r in repo_list:
+                existed_r = existed_repositories.get(r['name'])
+                if existed_r:
+                    existed_r.write(r)
+                else:
+                    request.env['github_project.repository'].create(r)
 
-            return "Get repositories success"
-        else:
-            return "You not config Github-Webhook!"
+        return "Success!"
 
     @http.route(['/github/callback'], type='http', auth="public", methods=['GET'], website=True)
     def callback(self, **kwargs):
