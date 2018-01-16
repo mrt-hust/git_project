69b5cd4d254fcee603426b13d458c868e74f1c8c
AceXXX <ace@trueplus.vn>
2018-01-16 19:13:56 +0700

    fix

--- github_project/controllers/github.py
+++ github_project/controllers/github.py
@@ -109,6 +109,5 @@ class GithubController(http.Controller):
     @http.route(['/repo/callback'], type='json', auth="public", methods=['POST'])
     def repo_callback(self, **kwargs):
         print(kwargs)
-        
         print(request.zen, request.data, request.body, request.params, request.query)
         return 'hello'
