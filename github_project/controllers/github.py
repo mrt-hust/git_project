f851160e8e71ce7a8047525812aa56beb24385f6
AceXXX <ace@trueplus.vn>
2018-01-16 14:01:34 +0700

    fix

--- github_project/controllers/github.py
+++ github_project/controllers/github.py
@@ -20,16 +20,23 @@ class GithubController(http.Controller):
         state = kwargs.get('state')
         web_hook = request.env['github_project.web_hook'].search([], limit=1)
         web_hook = web_hook[0] if len(web_hook) > 0 else False
-        print(web_hook.client_id)
-        print(web_hook)
+
         if web_hook and code and state:
             url = request.env['ir.config_parameter'].sudo().get_param('web.base.url') + \
                 '/github/callback?code=' + str(code) + '&state=' + str(state)
             url = url.replace('http', 'https')
-            print(url)
-            print(web_hook.client_id)
-            github = OAuth2Session(web_hook.client_id)
-            access_token = github.fetch_token(web_hook.token_url, client_secret=web_hook.client_secret,
-                                              authorization_response=url)
-            print(access_token)
+            try:
+                github = OAuth2Session(web_hook.client_id)
+                access_token = github.fetch_token(web_hook.token_url, client_secret=web_hook.client_secret,
+                                                  authorization_response=url)
+            except Exception as e:
+                return str(e)
+
+            access_token = access_token.get('access_token', '')
+            if access_token:
+                request.env.user.write({'github_access_token': access_token})
+                query_access_token = '?access_token=' + str(access_token)
+                api_domain = 'https://api.github.com/'
+                res = github.get(api_domain + 'user' + query_access_token)
+                print(res.content)
         return 'hello'
